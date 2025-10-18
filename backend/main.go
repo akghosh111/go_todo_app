60369beff81eb6e7181a595aa17b876e9e64cfc3
akghosh111 <ghoshkiran2017@gmail.com>
2025-10-18 14:48:15 +0530

    added api healthcheck route

--- backend/main.go
+++ backend/main.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+type HealthResponse struct {
+	Status  string `json:"status"`
+	Message string `json:"message"`
+}
+
+func healthHandler(w http.ResponseWriter, r *http.Request) {
+	health := HealthResponse{
+		Status:  "Ok",
+		Message: "API health is ok",
+	}
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(health)
+}
 
 func main() {
-	fmt.Println("Hello world")
+	http.HandleFunc("/health", healthHandler)
+
+	fmt.Println("App is running in PORT 3000")
+
+	err := http.ListenAndServe(":3000", nil)
+
+	if err != nil {
+		fmt.Println("Error in starting the application", err)
+	}
 }
